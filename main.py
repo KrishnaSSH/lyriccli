5bfb6d2e955b038708f20206cc923a661b432025
KrishnaSSH <kwynx@tuta.io>
2026-01-18 16:55:03 +0530

    tracks song_id and changes the lyrics when the song changes

--- main.py
+++ main.py
@@ -9,32 +9,41 @@ from core.formatter import format_timestamp
 console = Console(color_system="truecolor")
 
 
-song = cmus_current_song()
-if not song:
-    console.print("[bold red]No song currently playing!")
-    exit()
-
-lyrics = fetch_lyrics(song)
-plain_lyrics = lyrics["plainLyrics"]
-synced_lyrics = lyrics["syncedLyrics"]
-lyrics_tuples = []
-if synced_lyrics:
-    for line in synced_lyrics.splitlines():
-        if line.startswith("[") and "]" in line:
-            timestamp, lyric = (
-                line[1 : line.find("]")],
-                line[line.find("]") + 1 :].strip(),
-            )
-            try:
-                minutes, seconds = timestamp.split(":")
-                total_seconds = int(minutes) * 60 + float(seconds)
-                lyrics_tuples.append((total_seconds, lyric))
-            except ValueError:
-                continue
-
 last_line = None
+last_song_id = None
+lyrics_tuples = []
+
 while True:
     try:
+        song = cmus_current_song()
+        if not song:
+            console.print("[bold red]No song currently playing!")
+            exit()
+        song_id = (song.artist, song.title)
+        if song_id != last_song_id:
+            console.clear()
+            lyrics_tuples = []
+
+            lyrics = fetch_lyrics(song)
+            synced_lyrics = lyrics["syncedLyrics"]
+
+            if synced_lyrics:
+                for line in synced_lyrics.splitlines():
+                    if line.startswith("[") and "]" in line:
+                        timestamp, lyric = (
+                            line[1 : line.find("]")],
+                            line[line.find("]") + 1 :].strip(),
+                        )
+                        try:
+                            minutes, seconds = timestamp.split(":")
+                            total_seconds = int(minutes) * 60 + float(seconds)
+                            lyrics_tuples.append((total_seconds, lyric))
+                        except ValueError:
+                            pass
+            last_song_id = song_id
+            last_line = None
+            console.print(f"[bold green]Now playing:[/] {song_id}")
+
         position = cmus_current_position()
         current_line = ""
         for t, lyric in lyrics_tuples:
@@ -46,10 +55,10 @@ while True:
         if current_line != last_line:
             ts_formatted = format_timestamp(position)
             console.print(
-                f"[bold yellow]{ts_formatted} [bold white]{current_line}", end="\r"
+                f"[bold yellow]{format_timestamp(position)} [bold white]{current_line}"
             )
             last_line = current_line
         time.sleep(1)
     except KeyboardInterrupt:
-        console.print("\n[bold yellow] bye")
+        console.print("\n[bold yellow] :wave: bye")
         exit()
