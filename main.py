90cdfb254ab3c48dcc9240141aeb0076745a49ca
KrishnaSSH <kwynx@tuta.io>
2026-01-18 17:16:07 +0530

    change the time.sleep from 1 second to 0.5 to make it more real time

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ while True:
                 f"[bold yellow]{format_timestamp(position)} [bold white]{current_line}"
             )
             last_line = current_line
-        time.sleep(1)
+        time.sleep(0.5)
     except KeyboardInterrupt:
         console.print("\n[bold yellow] :wave: bye")
         exit()
