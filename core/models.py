75efd1d880ba72f221402a614ed27d4d77083394
KrishnaSSH <kwynx@tuta.io>
2026-01-19 16:57:40 +0530

    refactor: convert Song model to dataclass

--- core/models.py
+++ core/models.py
@@ -1,11 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Song:
-    def __init__(
-        self, file_name, title, artist, album_name, album_artist, genre, song_duration
-    ):
-        self.file_name = file_name
-        self.title = title
-        self.album_name = album_name
-        self.artist = artist
-        self.album_artist = album_artist
-        self.genre = genre
-        self.song_duration = song_duration
+    file_name: str
+    title: str
+    album_name: str
+    artist: str
+    album_artist: str
+    genre: str
+    song_duration: int
